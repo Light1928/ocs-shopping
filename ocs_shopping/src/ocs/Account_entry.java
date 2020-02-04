26fbf34552f61b5386ea055a0058f30a076edeed
Light1928 <51193957+Light1928@users.noreply.github.com>
2020-02-04 12:27:54 +0900

    サンプル

---
+++ ocs_shopping/src/ocs/Account_entry.java
@@ -0,0 +1,51 @@
+package ocs;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Account_entry
+ */
+@WebServlet("/Account_entry")
+public class Account_entry extends HttpServlet {
+
+	public static final String HOST_NAME = "10.15.121.37:3306";
+	public static final String DB_NAME   = "webapp2019_184040";
+	public static final String USER_NAME = "user_184040";
+	public static final String USER_PASS = "184040";
+	
+	@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+			int a = Integer.parseInt(request.getParameter(""));
+			String b = request.getParameter("");
+			int c = Integer.parseInt(request.getParameter(""));
+			String d = request.getParameter("");
+			int ret = 0;
+			try {
+				Class.forName("org.mariadb.jdbc.Driver");
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}
+			try(Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://" + HOST_NAME + "/" + DB_NAME + "?serverTimezone=JST",
+					USER_NAME, USER_PASS);
+					Statement stmt = conn.createStatement()){
+					 ret = stmt.executeUpdate("INSERT INTO kokyakutable"
+							+" VALUES("+ a +",'"+ b +"',"+ c +" , '"+ d +"')");
+			}catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+		}
+		
+
+	}
