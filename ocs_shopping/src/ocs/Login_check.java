1f5f23cd3e6aca44827db55cc9e95aacf777dd55
Light1928 <51193957+Light1928@users.noreply.github.com>
2020-02-04 12:23:28 +0900

    Login_check add

---
+++ ocs_shopping/src/ocs/Login_check.java
@@ -0,0 +1,124 @@
+package ocs;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login_check
+ */
+@WebServlet("/Login_check")
+public class Login_check extends HttpServlet {
+	public static final String HOST_NAME = "10.15.121.37:3306";
+	public static final String DB_NAME   = "webapp2019_OCSshop";
+	public static final String USER_NAME = "user_OCSshop";
+	public static final String USER_PASS = "OCSshop";
+	private final String URL = "jdbc:mysql://" + HOST_NAME + "/" + DB_NAME + "?serverTimezone=JST";
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("userid");
+		String password = request.getParameter("password");
+		
+		System.out.println("ID \t\t:[" + id + "]");
+
+		System.out.println("PASSWORD \t:[" + password + "]");
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	protected boolean user(String id, String pass) {
+
+		// nullのとき
+
+		if (id == null) {
+
+			System.out.println("id is null value.");
+
+			return false;
+
+		}
+
+		// lengthが0のとき
+
+		if (id.length() == 0) {
+
+			System.out.println("id is empty.");
+
+			return false;
+
+		}
+
+		if(pass == null) {
+
+			System.out.println("password is null value.");
+
+			return false;
+
+		}
+
+		if (pass.length() == 0) {
+
+			System.out.println("password is empty.");
+
+			return false;
+
+		}
+
+		try {
+
+			String sql = "select * from " + USER_NAME + " where id = ? and password = ?";
+
+		//	Class.forName("com.mysql.cj.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection(URL, "USER_NAME","USER_PASS");
+
+
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, id);
+
+			stmt.setString(2, pass);
+
+			ResultSet rs = stmt.executeQuery();
+
+
+
+			if (rs.next()) {
+
+				System.out.println("exist ID/PASSWORD pattern.");
+
+				return true;
+
+			} else {
+
+				System.out.println("doesn't exist ID/PASSWORD pattern.");
+
+				return false;
+
+			}
+
+		} catch (Exception ex) {
+
+			String msg = "ドライバのロードに失敗しました";
+
+			System.out.println(ex);
+
+			return false;
+
+		}
+	}
+
+}
