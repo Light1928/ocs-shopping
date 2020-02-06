a12bf7dbec6e3c4154a371378dfbec04b3865456
Light1928 <51193957+Light1928@users.noreply.github.com>
2020-02-06 11:02:25 +0900

    add

--- ocs_shopping/src/ocs/Ocs_Bean.java
+++ ocs_shopping/src/ocs/Ocs_Bean.java
@@ -6,7 +6,9 @@ public class Ocs_Bean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private String name;
-
+	private String goodsname;
+	private String price;
+	private String quantity;
 	//Constructor
 	public Ocs_Bean() {
 
@@ -18,5 +20,11 @@ public class Ocs_Bean implements Serializable{
 
 	public String getName() {
 		return this.name;
+	}
+	
+	public
+	
+	public String getCart() {
+		
 	}
 }
