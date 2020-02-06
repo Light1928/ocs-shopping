d7ac7276efdeb56dd0556dd8a6fd1784ac5a4738
Light1928 <51193957+Light1928@users.noreply.github.com>
2020-02-06 11:37:49 +0900

    cartBean

--- ocs_shopping/src/ocs/Ocs_Bean.java
+++ ocs_shopping/src/ocs/Ocs_Bean.java
@@ -6,9 +6,9 @@ public class Ocs_Bean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private String name;
-	private String goodsname;
-	private int price;
-	private int quantity;
+//	private String goodsname;
+//	private int price;
+//	private int quantity;
 	//Constructor
 	public Ocs_Bean() {
 
@@ -22,28 +22,28 @@ public class Ocs_Bean implements Serializable{
 		return this.name;
 	}
 	
-	public void setGoodsname(String goodsname) {
-		this.goodsname = goodsname;
-	}
-	
-	public String getGoodsname() {
-		return this.goodsname;
-	}
-	
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	
-	public int getPrice() {
-		return this.price;
-	}
-	
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
-	public int getQuantity() {
-		return this.quantity;
-	}
+//	public void setGoodsname(String goodsname) {
+//		this.goodsname = goodsname;
+//	}
+//	
+//	public String getGoodsname() {
+//		return this.goodsname;
+//	}
+//	
+//	public void setPrice(int price) {
+//		this.price = price;
+//	}
+//	
+//	public int getPrice() {
+//		return this.price;
+//	}
+//	
+//	public void setQuantity(int quantity) {
+//		this.quantity = quantity;
+//	}
+//	
+//	public int getQuantity() {
+//		return this.quantity;
+//	}
 }
 
