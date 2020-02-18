18b45240b28ee11326baec223318c6e641bee325
Light1928 <51193957+Light1928@users.noreply.github.com>
2020-02-18 10:53:06 +0900

    あ

--- ocs_shopping/src/ocs/CartInfoBean.java
+++ ocs_shopping/src/ocs/CartInfoBean.java
@@ -4,18 +4,18 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class CartInfoBean implements Serializable {
-	
+
 	private ArrayList<CartRecordBean> CartInfoArray = new ArrayList<CartRecordBean>();
-	
+
 	//constructor
 	public CartInfoBean() {
-		
+
 	}
-	
+
 	public ArrayList<CartRecordBean> getCartlistArray(){
 		return CartInfoArray;
 	}
-	
+
 	public void addCartlist(CartRecordBean obj) {
 		CartInfoArray.add(obj);
 	}
